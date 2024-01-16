a04b7cc52aff96ca6d92ab5c323f9544efd83a59
yunshin <2younsin@gmail.com>
2024-01-16 16:24:45 +0900

    config: eslint, prettier 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	resolve: {
+		alias: [
+			{ find: '@src', replacement: resolve(__dirname, 'src') },
+			{
+				find: '@components',
+				replacement: resolve(__dirname, 'src/components')
+			}
+		]
+	},
+
+	plugins: [react(), tsconfigPaths()]
 })
