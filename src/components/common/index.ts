445b236cedf869f6300fcb880c607cf57ba428a4
yunshin <2younsin@gmail.com>
2024-02-22 06:45:15 +0900

    design: ShupickerIcon 컴포넌트 작성

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,4 +1,5 @@
 import Box from './box'
 import Header from './header'
 import PersonnelList from './personnel-list'
-export { Box, Header, PersonnelList }
+import ShupickerIcon from './shpicker-icon'
+export { Box, Header, PersonnelList, ShupickerIcon }
---
+++ src/components/common/shpicker-icon/shupicker-icon.type.ts
@@ -0,0 +1,7 @@
+import { IconProp, SizeProp } from '@fortawesome/fontawesome-svg-core'
+
+export type ShupickerIconProps = {
+	iconData: IconProp
+	color: string
+	size: SizeProp
+}
