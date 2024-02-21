492417cb8dc75e1d710c687574627b6bc1b75b82
yunshin <2younsin@gmail.com>
2024-02-21 20:37:54 +0900

    config: recoil 의존성 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RecoilRoot } from 'recoil'
 import App from './App.tsx'
 import GlobalStyles from './libs/styled-components/global.tsx'
 
@@ -13,7 +14,9 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<GlobalStyles />
-		<RouterProvider router={router} />
+		<RecoilRoot>
+			<GlobalStyles />
+			<RouterProvider router={router} />
+		</RecoilRoot>
 	</React.StrictMode>
 )
