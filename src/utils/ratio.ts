20b6ab20bf15ee4f79ecba0497cf9189fd4bb4de
yunshin <2younsin@gmail.com>
2024-03-20 23:08:12 +0900

    chore: 배포 오류수정

--- src/utils/ratio.ts
+++ src/utils/ratio.ts
@@ -1,5 +1,5 @@
-/**
- * 비율구하기
- * - 반환형식: [분자, 분모]
- */
-const getParticipationRatio = (memberArrayd) => {}
+// /**
+//  * 비율구하기
+//  * - 반환형식: [분자, 분모]
+//  */
+// const getParticipationRatio = (memberArrayd) => {}
