1e97949cdb6b983297b0fa1e041dd19dbf7548b9
yunshin <2younsin@gmail.com>
2024-04-04 15:39:25 +0900

    refactor: 암호화 방식 변경 (aes->base64)

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,16 +1,5 @@
-import CryptoJS from 'crypto-js'
+export const arrayEncoder = <T>(originArray: T[]): string =>
+	btoa(encodeURIComponent(JSON.stringify(originArray)))
 
-export const arrayEncoder = <T>(originArray: T[]): string => {
-	const jsonString = JSON.stringify(originArray)
-	const encryptedString = CryptoJS.AES.encrypt(
-		jsonString,
-		import.meta.env.VITE_APP_CRYPTO_SALT
-	).toString()
-	return encryptedString
-}
-export const arrayDecoder = <T>(encryptedString: string): T[] => {
-	const bytes = CryptoJS.AES.decrypt(encryptedString, import.meta.env.VITE_APP_CRYPTO_SALT)
-	const decryptedString = bytes.toString(CryptoJS.enc.Utf8)
-	const originArray = JSON.parse(decryptedString)
-	return originArray
-}
+export const arrayDecoder = <T>(encodedString: string): T[] =>
+	JSON.parse(decodeURIComponent(atob(encodedString)))
