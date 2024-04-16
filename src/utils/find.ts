b1f29a8ff3ca88c79fecdde5fdedf5b95d5e4c27
yunshin <2younsin@gmail.com>
2024-04-16 22:54:22 +0900

    feat: 객체 내부에 특정 속성을 찾아내는 메서드 파일 추가

---
+++ src/utils/find.ts
@@ -0,0 +1,9 @@
+import type { MemberTableType, MemberType, TraitType } from '@/types'
+export const findEssentialTraitById = (member: MemberType, traitId: number) => {
+	const found = member?.essentialTraits.find((trait) => trait.id === traitId) as TraitType
+	return found!.value
+}
+export const findMemberById = (memberArray: MemberTableType, memberId: number) => {
+	const found = memberArray.find((member) => member.id === memberId)
+	return found
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 export * from './crypto'
+export * from './find'
 export * from './range'
 export * from './sort'
 export * from './time'
