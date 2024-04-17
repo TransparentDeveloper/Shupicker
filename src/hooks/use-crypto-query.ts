e094636a13cfc85478e5d2cb3d9e6df38e302af1
yunshin <2younsin@gmail.com>
2024-04-17 20:47:46 +0900

    feat: url 로 관리되는 배열 데이터에 새 요소 삽입 메서드 작성

--- src/hooks/use-crypto-query.ts
+++ src/hooks/use-crypto-query.ts
@@ -3,21 +3,30 @@ import { useSearchParams } from 'react-router-dom'
 
 export const useCryptoQuery = () => {
 	const [params, setParams] = useSearchParams()
-
 	const getPrimitive = (paramKey: string, notFound: number | string): number | string => {
 		if (!params.has(paramKey)) return notFound
 		const found = params.get(paramKey)
 		const decoded = decoder<number | string>(found as string)
 		return decoded as number | string
 	}
-
+	const setPrimitive = (paramKey: string, newPrimitive: number | string) => {
+		const getOne = getPrimitive(paramKey, -1)
+		if (getOne === newPrimitive) return
+		params.set(paramKey, encoder(newPrimitive))
+		setParams(params)
+	}
 	const getArray = <T>(paramKey: string): Array<T> => {
 		if (!params.has(paramKey)) return []
 		const found = params.get(paramKey)
 		const decoded = decoder<T>(found as string)
 		return decoded as Array<T>
 	}
-
+	const appendArray = <T extends Object>(paramKey: string, newElem: T) => {
+		const originalArr = getArray<T>(paramKey)
+		const newArr = [...originalArr, newElem]
+		params.set(paramKey, encoder(newArr))
+		setParams(params)
+	}
 	const setMultiQueryData = (
 		paramKeyArray: Array<string>,
 		paramValueArray: Array<Array<Object> | string | number>
@@ -25,15 +34,12 @@ export const useCryptoQuery = () => {
 		const keyLength = paramKeyArray.length
 		const valueLength = paramValueArray.length
 		if (keyLength !== valueLength) throw new Error('Key and value pair count mismatch.')
-		for (let i = 0; i < keyLength; i++) {
-			params.set(paramKeyArray[i], encoder(paramValueArray[i]))
-		}
+		for (let i = 0; i < keyLength; i++) params.set(paramKeyArray[i], encoder(paramValueArray[i]))
 		setParams(params)
 	}
-
 	const removeQueryData = (paramKey: string) => {
 		if (params.has(paramKey)) params.delete(paramKey)
 	}
 
-	return { getPrimitive, getArray, setMultiQueryData, removeQueryData }
+	return { getPrimitive, setPrimitive, getArray, setMultiQueryData, removeQueryData, appendArray }
 }
