07107134f3add433b595d7d5a40b20d868477ed1
yunshin <2younsin@gmail.com>
2024-04-21 00:00:38 +0900

    feat : 타입 명시

--- src/utils/find.ts
+++ src/utils/find.ts
@@ -3,7 +3,8 @@ export const findEssentialTraitById = (member: MemberType, traitId: number) => {
 	const found = member.essentialTraits.find((trait) => trait.id === traitId) as TraitType
 	return found.value
 }
-export const findMemberById = (memberArray: MemberTableType, memberId: number) => {
+export const findMemberById = (memberArray: MemberTableType, memberId: number): MemberType => {
 	const found = memberArray.find((member) => member.id === memberId)
+	if (!!!found) throw Error('Member not found.')
 	return found
 }
