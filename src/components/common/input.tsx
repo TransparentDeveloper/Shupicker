58a3dcfdc4a41f2e6207d43aa2373f16d6a7c5c0
yunshin <2younsin@gmail.com>
2024-06-17 02:34:42 +0900

    design: sheet 컴포넌트 작성

---
+++ src/components/common/input.tsx
@@ -0,0 +1,22 @@
+import {cn} from '@/libs/shadcn/util'
+import {forwardRef} from 'react'
+
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+	({className, type, ...props}, ref) => {
+		return (
+			<input
+				type={type}
+				className={cn(
+					'flex h-14 w-full items-center rounded-md border border-gray-500 bg-black px-3 py-2 text-base text-gray-200 placeholder:text-base placeholder:text-gray-500 focus:border-gray-300 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+					className,
+				)}
+				ref={ref}
+				{...props}
+			/>
+		)
+	},
+)
+
+Input.displayName = 'Input'
