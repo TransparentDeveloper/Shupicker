ee2463c5b43c9f9905f8268fca1c0cd30aaec8f1
yunshin <2younsin@gmail.com>
2024-06-16 16:31:22 +0900

    chore: 불필요 shadcn 컴포넌트 삭제

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,1 @@
-export * from './card'
-export * from './context-menu'
 export * from './scroll-area'
-export * from './table'
--- src/sections/registration/index.tsx
+++ src/sections/registration/index.tsx
@@ -1,73 +1,59 @@
-import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
-	ScrollArea,
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableHeader,
-	TableRow,
-} from '@/components/ui'
+import {ScrollArea} from '@/components/ui'
 import {PlusSquareIcon} from 'lucide-react'
 
 export const RegisterSection = () => {
 	return (
-		<Card className='h-full w-full overflow-hidden'>
-			<CardHeader className='pb-2'>
-				<CardTitle>회원 등록</CardTitle>
-				<CardDescription>회원 및 속성을 등록하세요.</CardDescription>
-			</CardHeader>
-			<CardContent className='relative h-4/6 w-full'>
-				<div className='sticky top-0 z-10'>
-					<Table>
-						<TableHeader>
-							<TableRow>
-								<TableHead className='w-[150px] text-center'>이름</TableHead>
-								<TableHead className='text-center'>등록시간</TableHead>
-								<TableHead className='text-right'>게임횟수</TableHead>
-							</TableRow>
-						</TableHeader>
-					</Table>
-				</div>
-				<ScrollArea className='h-[calc(100%-64px)] w-full'>
-					<Table>
-						<TableBody>
-							{Array.from({length: 8}).map((_, idx) => (
-								<MemberInfoOne key={idx} />
-							))}
-						</TableBody>
-					</Table>
+		<div className='grid h-full w-full grid-rows-[65px_1fr_48px] gap-[0px] p-6'>
+			<CardHeader title='회원 등록' description='회원 및 속성을 등록하세요.' />
+			<table
+				className='flex h-full flex-col items-start overflow-hidden'
+				width='100%'
+			>
+				<tr className='grid h-[70px] w-full grid-cols-[1fr_1fr_1fr_1fr] items-center justify-center rounded-none border-b-[2px] p-2 text-center'>
+					<th>이름</th>
+					<th>등록시간</th>
+					<th></th>
+					<th className='text-end'>게임 횟수</th>
+				</tr>
+
+				<ScrollArea className='h-full w-full'>
+					<tr className='grid h-[55px] w-full grid-cols-[1fr_1fr_1fr_1fr] items-center justify-center rounded-none border-b-[2px] p-2 text-center'>
+						<td>이윤신</td>
+						<td>ㅁㄴㅇ라</td>
+						<td></td>
+						<td className='text-end'>ㅁㄴㅇㄹ</td>
+					</tr>
 				</ScrollArea>
-			</CardContent>
-			<CardFooter>
-				<OpenModalButton />
-			</CardFooter>
-		</Card>
+			</table>
+			<AddMemberButton />
+		</div>
 	)
 }
 
-const MemberInfoOne = () => {
+const AddMemberButton = () => {
 	return (
-		<TableRow>
-			<TableCell className='w-[150px] text-center font-medium'>1</TableCell>
-			<TableCell className='text-center'>20:00</TableCell>
-			<TableCell className='text-right'>10 회</TableCell>
-		</TableRow>
-	)
-}
-
-const OpenModalButton = () => {
-	return (
-		<button className='flex h-12 w-full cursor-pointer items-center justify-center'>
+		<button className='flex h-[48px] w-full cursor-pointer items-center justify-center'>
 			<div className='flex h-full w-fit min-w-[50%] border-spacing-28 items-center justify-center gap-2 rounded-lg border-[3px] border-dotted border-[#777777] text-[#777777] transition-colors duration-200 hover:bg-white'>
 				<PlusSquareIcon color='#777777' className='bg-transparent' />
 				추가하기
 			</div>
 		</button>
+	)
+}
+
+const CardHeader = ({
+	title,
+	description,
+}: {
+	title: string
+	description: string
+}) => {
+	return (
+		<header className='flex h-[65px] flex-col justify-start gap-2'>
+			<h3 className='text-2xl font-semibold leading-none tracking-tight'>
+				{title}
+			</h3>
+			<p className='text-base text-gray-500'>{description}</p>
+		</header>
 	)
 }
