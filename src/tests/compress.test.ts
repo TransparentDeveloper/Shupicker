ebd0192c07c59fd5cdcb3f05b90e2bfcc2cd98d3
yunshin <2younsin@gmail.com>
2024-08-19 15:29:31 +0900

    remove: 유틸리티 함수 제거

--- src/tests/compress.test.ts
+++
@@ -1,9 +0,0 @@
-import {compress, decompress} from '@/utils'
-import {expect, test} from 'vitest'
-
-test('압축 전과 압축해제 후의 값이 일치해야 한다.', () => {
-	const originalString = 'originalString'
-	const compressed = compress(originalString)
-	const decompressed = decompress(compressed)
-	expect(originalString).toBe(decompressed)
-})
